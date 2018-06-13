0a666da81f9990d052d1066495e36fa0a1224731
majed187 <majedshrndan45@gmail.com>
2018-06-13 02:02:28 +0200

    declert object varibale

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ function getRandomInt(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+let dice = {
+	value: 1,
+	roll: () => {
+		this.value = getRandomInt(1, 6);
+		return this.value;
+
+	}
+};
+
+let dice2 = {
+	value: 1,
+	roll: () => {
+		this.value = getRandomInt(1, 6);
+		return this.value;
+
+	}
+};
